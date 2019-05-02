f92344e3db8b0c292b959112981cf5e104a2d117
Yu Jeonghoon <moning02004@naver.com>
2019-05-02 02:14:05 +0000

    14 : Server json loads

--- Server/talsuk_e/app_statistics/views.py
+++ Server/talsuk_e/app_statistics/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db.models import Sum
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from collections import OrderedDict
 
@@ -7,10 +8,14 @@ from .models import Year, Month, Day
 import json
 
 
+@csrf_exempt
 def me(request):
+    data = json.loads(request.body.decode('utf-8'))
+    username = data['username']
+    print(username)
     stat = OrderedDict()
     stat['year'] = []
-    years = Year.objects.all()
+    years = Year.objects.all().filter(user=User.objects.get(username=username))
     for year in years:
         stat_year = OrderedDict()
         stat_year['num'] = year.num
@@ -28,31 +33,36 @@ def me(request):
                 stat_day['weight'] = day.weight
                 stat_day['fee'] = day.fee
                 stat_month['day'].append(stat_day)
-                print(json.dumps(stat_month, ensure_ascii=False, indent='\t'))
             stat_year['month'].append(stat_month)
         stat['year'].append(stat_year)
 
     print(json.dumps(stat, ensure_ascii=False, indent='\t'))
-    return JsonResponse(json.dumps(stat, ensure_ascii=False), safe=False)
+    return JsonResponse(stat, safe=False)
 
 
+@csrf_exempt
 def local(request):
     return JsonResponse({})
 
 
+@csrf_exempt
 def save(request):
-    username = request.user
-    print(username)
-    year = 2019
-    num = 5
-    year = Year.objects.get(num=year) if Year.objects.all().filter(num=year).exists() else Year(num=year, user=User.objects.get(username=username))
-    year.save()
-    month = Month.objects.get(year=year, num=num) if Month.objects.all().filter(year=year, num=num).exists() else Month(year=year, num=num)
-    month.save()
-    day = Day()
-    day.num = 1
-    day.month = month
-    day.fee = 50
-    day.weight = 450
-    day.save()
-    return JsonResponse({})
+    data = json.loads(request.body.decode('utf-8'))
+    username = data['username']
+    print(data['username'])
+    year = int(data['year'])
+    month = int(data['month'])
+    day = int(data['day'])
+
+    new_year = Year.objects.get(num=year, user=User.objects.get(username=username)) if Year.objects.all().filter(user=User.objects.get(username=username), num=year).exists() else Year(num=year, user=User.objects.get(username=username))
+    new_year.save()
+    new_month = Month.objects.get(year=new_year, num=month) if Month.objects.all().filter(year=new_year, num=month).exists() else Month(year=new_year, num=month)
+    new_month.save()
+    
+    new_day = Day()
+    new_day.num = day
+    new_day.month = new_month
+    new_day.fee = 50
+    new_day.weight = 450
+    new_day.save()
+    return JsonResponse({'message':'Success'})
