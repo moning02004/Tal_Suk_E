398a233151e028ef7a58d2651757aa2cc277279c
Yu Jeonghoon <moning02004@naver.com>
2019-05-22 10:30:15 +0000

    34 : Server remove local statistics

--- Server/talsuk_e/app_info/views.py
+++ Server/talsuk_e/app_info/views.py
@@ -2,32 +2,35 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-from app_user.models import SukE
 from .models import Info
 import json
 
 
 @csrf_exempt
 def index(request):
-    print("AAAAA")
-    data = json.loads(request.body.decode('utf-8'))
-    print(data)
-    message = dict()
-    message['info'] = []
-    for info in reversed(Info.objects.all()):
-        info_json = dict()
-        info_json['title'] = info.title
-        info_json['content'] = info.content
-        info_json['created'] = (info.created).strftime("%Y.%m.%d %H:%M")
-        message['info'].append(info_json)
-    return JsonResponse(message, safe=False)
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+        message = dict()
+        message['info'] = []
+        for info in reversed(Info.objects.all()):
+            info_json = dict()
+            info_json['title'] = info.title
+            info_json['content'] = info.content
+            info_json['created'] = (info.created).strftime("%Y.%m.%d %H:%M")
+            message['info'].append(info_json)
+        return JsonResponse(message, safe=False)
+    except:
+        return JsonResponse({'message': 'Fail'})
 
 
 @csrf_exempt
 def new(request):
-    data = json.loads(request.body.decode('utf-8'))
-    info = Info()
-    info.title = data['title']
-    info.content = data['content']
-    info.save()
-    return JsonResponse({'message':'Success'})
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+        info = Info()
+        info.title = data['title']
+        info.content = data['content']
+        info.save()
+        return JsonResponse({'message': 'Success'})
+    except:
+        return JsonResponse({'message': 'Fail'})
--- Server/talsuk_e/app_statistics/urls.py
+++ Server/talsuk_e/app_statistics/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name='app_statistics'
 urlpatterns = [
-    path('__me_/', views.me, name='me'),
-    path('__local_/', views.local, name='local'),
-    path('__save_/', views.save, name='save'),
+    path('__me_/', views.me),
+    path('temp/', views.temp),
 ]
+
