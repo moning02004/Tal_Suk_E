764c65d1f02085d43f931c7357e1646ab5f3ac29
Yu Jeonghoon <moning02004@naver.com>
2019-05-22 10:25:17 +0000

    33 : Server edit user model, url, views

--- Server/talsuk_e/app_user/models.py
+++ Server/talsuk_e/app_user/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class SukE(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address1 = models.CharField(max_length=200)
-    address2 = models.CharField(max_length=200)
     name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.user.username
+
--- Server/talsuk_e/app_user/urls.py
+++ Server/talsuk_e/app_user/urls.py
@@ -6,6 +6,8 @@ app_name='app_user'
 urlpatterns = [
     path('__login_/', views.login),
     path('__register_/', views.register),
+    path('__edit_/', views.edit),
     path('__check_/<str:pk>/', views.check),
-    path('__leave_/<str:pk>/', views.leave)
+    path('__leave_/', views.leave)
 ]
+
