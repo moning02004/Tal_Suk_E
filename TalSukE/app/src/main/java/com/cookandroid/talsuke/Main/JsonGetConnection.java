151580cf2d5ece5e9ae6d452b692cd4c7c7dc59d
junhiKim <tjsej1324@gmail.com>
2019-05-29 18:08:40 +0900

    40 : 무게 받기

---
+++ TalSukE/app/src/main/java/com/cookandroid/talsuke/Main/JsonGetConnection.java
@@ -0,0 +1,52 @@
+package com.cookandroid.talsuke.Main;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JsonGetConnection extends AsyncTask<JSONObject, JSONObject, JSONObject> {
+    private HttpURLConnection connection;
+
+    public JsonGetConnection(String url) throws IOException {
+        URL urls = new URL(url);
+        connection = (HttpURLConnection) urls.openConnection();
+    }
+
+    @Override
+    protected JSONObject doInBackground(JSONObject[] json) {
+        JSONObject response = null;
+        try {
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(false);
+            connection.connect();
+
+            response = this.getResponse();
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null)
+                connection.disconnect();
+        }
+        return response;
+    }
+
+    private JSONObject getResponse() throws JSONException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line = "";
+        StringBuilder buffer = new StringBuilder();
+        while ((line = reader.readLine()) != null) buffer.append(line);
+
+        return new JSONObject(buffer.toString().trim());
+    }
+}
